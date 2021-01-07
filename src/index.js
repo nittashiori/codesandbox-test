d4db5b31cc30a20d0c5a55921c270e5cafbcd97d
nittashiori <30612104+nittashiori@users.noreply.github.com>
2021-01-07 11:04:19 +0900

    三項演算子の学習

--- src/index.js
+++ src/index.js
@@ -170,10 +170,29 @@ const nameArr = ["田中", "山田", "新田"];
 // });
 // console.log(newNumArr);
 
-const newNameArr = nameArr.map((name) => {
-  if (name === "新田") {
-    return name;
-  }
-  return `${name}さん`;
-});
-console.log(newNameArr);
+// const newNameArr = nameArr.map((name) => {
+//   if (name === "新田") {
+//     return name;
+//   }
+//   return `${name}さん`;
+// });
+// console.log(newNameArr);
+
+/**
+ * 三項演算子
+ */
+// ある条件 ? 条件がtrueの時 : 条件がfalseの時
+// const val1 = 1 < 0 ? 'trueです' : 'falseです';
+// console.log(val1);
+
+// const num = "1300";
+// // toLocaleString　...　数値を三桁ごとに,を追加する
+// // console.log(num.toLocaleString());
+// // typeof ... 値の型を判定する（数値、文字列etc）
+// const formattedNum = typeof num === 'number' ? num.toLocaleString() : '数値を入力してください';
+// console.log(formattedNum);
+
+const checkSum = (num1, num2) => {
+  return num1 + num2 > 100 ? "100を超えています!!" : "許容範囲内です";
+};
+console.log(checkSum(50, 40));
